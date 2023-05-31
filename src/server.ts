f273bcff69de1f6a53f147ca6711f1bbc6b4b8b2
Rayhan-abdullha <captainrayhan215@gmail.com>
2023-05-31 11:38:52 +0600

    setup eslint husky pretter lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function run() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log('Database connected..');
-        
-        app.listen(config.port, () => {
-          console.log(`Server is running on PORT ${config.port}`);
-          
-      })
-    } catch (err:any) {
-        console.log(err);
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected..')
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on PORT ${config.port}`)
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 run()
