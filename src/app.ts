d80a589797da62842ce912aa6eddc55410e008c1
Rayhan-abdullha <captainrayhan215@gmail.com>
2023-05-31 12:08:42 +0600

    create interface and model

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express, {Application, Request, Response} from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
 app.use([express.json(), cors()])
 
-app.get('/', (req: Request, res:Response) => {
-    res.send('hello')
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello')
 })
 
 export default app
